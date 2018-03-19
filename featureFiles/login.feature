536a7a6da814ae5d9f2b0336648beb142389e8f3
sathyasp <sathyatamilsellvan@gmail.com>
2018-03-19 16:22:07 +0530

    first commit

---
+++ featureFiles/login.feature
@@ -0,0 +1,6 @@
+Feature: User login test
+
+Scenario: User test the login with valid credentials
+
+Given: User is in login page
+when 
---
+++ src/main/java/steps/Stepdefinition.java
@@ -0,0 +1,26 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Stepdefinition {@Given("^User is in login page$")
+public void user_is_in_login_page() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^User enters username and password$")
+public void user_enters_username_and_password() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^user clicks Login button$")
+public void user_clicks_Login_button() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+}
